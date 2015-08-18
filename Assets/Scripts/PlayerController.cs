8ee6efb29ab11bc5aaa118e368b5cc3ce0891f73
Meg Drouhard <meg.drouhard@gmail.com>
2015-08-18 09:06:20 -0400

    Initial Commit; Working with main camera, response to input is slow

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,200 @@
+﻿/* Player Controller targeting Oculus VR
+ * Based on UnityStandardAssets First Person Controller */
+
+using UnityEngine;
+using System;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+
+[RequireComponent(typeof (Rigidbody))]
+[RequireComponent(typeof (CapsuleCollider))]
+public class PlayerController : MonoBehaviour {
+
+	[Serializable]
+	public class MovementSettings
+	{
+		public float ForwardSpeed = 8.0f;   // Speed when moving forward
+		public float BackwardSpeed = 4.0f;  // Speed when moving backwards
+		public float StrafeSpeed = 4.0f;    // Speed when moving sideways
+		public float RunMultiplier = 2.0f;   // Multiplier for speed when moving quickly
+		public KeyCode RunKey = KeyCode.LeftShift;
+		public AnimationCurve SlopeCurveModifier = new AnimationCurve(new Keyframe(-90.0f, 1.0f), new Keyframe(0.0f, 1.0f), new Keyframe(90.0f, 0.0f));
+		[HideInInspector] public float CurrentTargetSpeed = 8f;
+		
+		
+		#if !MOBILE_INPUT
+		private bool m_Running;
+		#endif
+		
+		public void UpdateDesiredTargetSpeed(Vector2 input)
+		{
+			if (input == Vector2.zero) return;
+			if (input.x > 0 || input.x < 0)
+			{
+				//strafe
+				CurrentTargetSpeed = StrafeSpeed;
+			}
+			if (input.y < 0)
+			{
+				//backwards
+				CurrentTargetSpeed = BackwardSpeed;
+			}
+			if (input.y > 0)
+			{
+				//forwards
+				//handled last because if strafing and moving forward at the same time forwards speed should take precedence
+				CurrentTargetSpeed = ForwardSpeed;
+			}
+			
+			
+			#if !MOBILE_INPUT
+			if (Input.GetKey(RunKey))
+			{
+				CurrentTargetSpeed *= RunMultiplier;
+				m_Running = true;
+			}
+			else
+			{
+				m_Running = false;
+			}
+			#endif
+		}
+		
+		#if !MOBILE_INPUT
+		public bool Running
+		{
+			get { return m_Running; }
+		}
+		#endif
+	}
+
+	[Serializable]
+	public class AdvancedSettings
+	{
+		public float slowDownRate = 20f; // rate at which the controller comes to a stop when there is no input
+	}
+
+	public Camera cam;
+	//public OVRCameraRig cam;
+	public GameObject reticle;
+	
+	public MovementSettings movementSettings = new MovementSettings();
+	public MouseLook mouseLook = new MouseLook();
+	public AdvancedSettings advancedSettings = new AdvancedSettings();
+	
+	
+	private Rigidbody m_RigidBody;
+	private float m_YRotation;
+	private bool m_Braking;
+	
+	
+	public Vector3 Velocity
+	{
+		get { return m_RigidBody.velocity; }
+	}
+	
+	public bool Running
+	{
+		get
+		{
+			#if !MOBILE_INPUT
+			return movementSettings.Running;
+			#else
+			return false;
+			#endif
+		}
+	}
+
+	// initialization
+	void Start () {
+		m_RigidBody = GetComponent<Rigidbody>();
+		//mouseLook.Init (transform, cam.centerEyeAnchor);
+		mouseLook.Init (transform, cam.transform);
+		m_Braking = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!reticle.activeSelf && (Input.GetButton("Fire1"))) {
+			reticle.SetActive(!reticle.activeSelf);
+		} else if(reticle.activeSelf && (Input.GetButton("Fire3"))) {
+			reticle.SetActive(!reticle.activeSelf);
+		}
+		
+		RotateView();
+	}
+
+	private void FixedUpdate()
+	{
+		Vector2 input = GetInput();
+		
+		// if braking, only brake
+		if (m_Braking) {
+			m_RigidBody.velocity = m_RigidBody.velocity * 0.9f;
+			m_RigidBody.angularVelocity = m_RigidBody.angularVelocity * 0.9f;
+			// else if a move is desired, apply appropriate force
+		} else if ((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) /*&& (advancedSettings.airControl || m_IsGrounded)*/)
+		{
+			// always move along the camera forward as it is the direction that it being aimed at
+			//Vector3 desiredMove = cam.centerEyeAnchor.forward*input.y + cam.centerEyeAnchor.right*input.x;
+			Vector3 desiredMove = cam.transform.forward*input.y + cam.transform.right*input.x;
+			
+			desiredMove.x = desiredMove.x*movementSettings.CurrentTargetSpeed;
+			desiredMove.z = desiredMove.z*movementSettings.CurrentTargetSpeed;
+			desiredMove.y = desiredMove.y*movementSettings.CurrentTargetSpeed;
+			
+			if (m_RigidBody.velocity.sqrMagnitude <
+			    (movementSettings.CurrentTargetSpeed*movementSettings.CurrentTargetSpeed))
+			{
+				m_RigidBody.AddForce(desiredMove, ForceMode.Impulse);
+//									Debug.Log("Trying to add force: " + desiredMove);
+			}
+		}
+		
+		// If input & previous velocity are nonexistent or negligible, sleep
+		if (Mathf.Abs(input.x) < float.Epsilon && Mathf.Abs(input.y) < float.Epsilon && m_RigidBody.velocity.magnitude < 1f)
+		{
+			m_RigidBody.Sleep();
+		}
+	}
+	
+	private Vector2 GetInput()
+	{
+		
+		Vector2 input = new Vector2
+		{
+			x = CrossPlatformInputManager.GetAxis("Horizontal"),
+			y = CrossPlatformInputManager.GetAxis("Vertical")
+		};
+		
+		if (input == Vector2.zero) {
+			m_Braking = true;
+		} else {
+			m_Braking = false;
+			movementSettings.UpdateDesiredTargetSpeed(input);
+		}
+		
+		return input;
+	}
+	
+	
+	private void RotateView()
+	{
+		//avoids the mouse looking if the game is effectively paused
+		if (Mathf.Abs(Time.timeScale) < float.Epsilon) return;
+		
+		//TODO: REMOVE?
+		// get the rotation before it's changed
+		//            float oldYRotation = transform.eulerAngles.y;
+		
+		//mouseLook.LookRotation (transform, cam.centerEyeAnchor);
+		mouseLook.LookRotation (transform, cam.transform);
+	}
+	
+	// Stop on collision (don't bounce)
+	void OnCollisionEnter(Collision collision) {
+		m_RigidBody.velocity = Vector3.zero;
+	}
+}
---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reticle : MonoBehaviour {
+
+	public GameObject targetObject;
+	public Material highlightMaterial;
+	public Camera cameraFacing = null;
+	public float fixedGazeSeconds = 2f;
+
+	private Vector3 originalScale;
+	private float timer;
+
+
+	// Initialization
+	void Start () {
+		originalScale = transform.localScale;
+		timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cameraFacing != null) {
+			RaycastHit hit;
+			float distance;
+			if (Physics.Raycast(new Ray(cameraFacing.transform.position, cameraFacing.transform.forward), 
+			                    out hit)) {
+				distance = hit.distance;
+
+				// update timer if object encountered with Raycast
+				timer += Time.deltaTime;
+
+				// Cross-platform input "Fire2" triggers location saving 
+				if( (Input.GetButton("Fire2")) &&
+				   ((hit.collider.transform.root == targetObject.transform) || (hit.collider.transform == targetObject.transform)) ) {
+					Highlight(hit.collider.gameObject);
+				}
+			} else {
+				// reset timer if no object encountered with Raycast
+				timer = 0f;
+				distance = cameraFacing.farClipPlane * 0.95f;
+			}
+		
+
+			transform.position = cameraFacing.transform.position + 
+			(cameraFacing.transform.forward * distance);
+			
+			this.transform.LookAt (cameraFacing.transform.position);
+			// rotate around y axis because the above has back of quad facing camera
+			transform.Rotate (0.0f, 180.0f, 0.0f);
+
+			// fix screen size of reticle (regardless of distance)
+			// if very close to reticle, make reticle appear slightly larger
+			if (distance < 10.0f) {
+				distance *= 1 + 5*Mathf.Exp(-distance);
+			}
+			transform.localScale = originalScale * distance;
+
+			// if gaze has fixed on an object for more seconds than fixedGazeSeconds
+			if (timer > fixedGazeSeconds) {
+				Highlight (hit.collider.gameObject);
+				timer = 0f;
+			}
+		}
+	}
+
+	// Animate Extended Gaze
+	private void AnimateExtendedGaze(GameObject gazeObject) {
+
+	}
+
+	// Highlight targeted game object
+	void Highlight(GameObject highlightObject) {
+		highlightObject.GetComponent<MeshRenderer> ().material = highlightMaterial;
+	}
+}
